3e19059be75aa2c4d6d17b67f7a4693b48733fb4
fearless <ghost@fearlesss-Mac-mini.local>
2022-06-01 14:10:52 +0800

    Fixed bug in lambda function no docker

--- lambda_app_no_docker.py
+++ lambda_app_no_docker.py
@@ -1,21 +1,20 @@
 from datetime import datetime
-
+import base64
+import json
 
 def lambda_handler(event, context):
     try:
-        time_string = event["time_string"]
+        try:
+            time_string = event["time_string"]
+        except KeyError:
+            response = json.loads(base64.b64decode(event["body"]).decode('utf-8'))
+            time_string = response["time_string"]
         datetime_now = datetime.now()
-        hour = int(time_string[0])
+        hour = int(time_string[:-2])
         time_string_datetime = datetime_now.replace(hour=hour,
                                                     minute=0,
                                                     second=0).strftime(
                                                     "%m-%d-%Y, %H:%M:%S %p")
-        return {
-            'statusCode': 200,
-            'body': time_string_datetime
-        }
+        return json.dumps({"response": time_string_datetime})
     except KeyError:
-        return {
-            'statusCode': 400,
-            'body': "needs time_string as request body"
-        }
+        return json.dumps({"response": "needs time_string as request body"})
